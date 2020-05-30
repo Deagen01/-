ea3898e5086cbc1284a02a4d48379b0ae8eea5e9
Deagen01 <45703188+Deagen01@users.noreply.github.com>
2020-05-30 16:56:30 +0800

    上网搜索单词信息返回一个词典

--- 软工第三次上机/second.py
+++ 软工第三次上机/second.py
@@ -3,49 +3,51 @@
 import requests
 from bs4 import BeautifulSoup
 
-# 输出单词词义
-def outPutTrans(soup):
-    s = soup.find(class_='trans-container')('ul')[0]('li')
-    for item in s:
-        if item.text:
-            print(item.text)
-# 输出例句及其翻译
-def outPutExamples(soup):
-    exList=soup.find(class_="trans-container tab-content",id="bilingual")('ul')[0]('li')
-    ex1=exList[0]('p')
-    print("例句")
-    print(ex1[0].get_text())
-    print("翻译")
-    print(ex1[1].get_text())
-def outPutPhonetic(soup):
-    s=soup.find(class_="phonetic")
-    print("发音")
-    print(s.get_text())
-def output(soup):
-    outPutTrans(soup)
-    outPutPhonetic(soup)
-    outPutExamples(soup)
-# get word from Command line
-word = input("Enter a word (enter 'q' to exit): ")
+class GetNewWord:
 
-# main body
-while word != 'q': # 'q' to exit
-    try:
-        # 利用GET获取输入单词的网页信息
-        r = requests.get(url='http://dict.youdao.com/w/%s/#keyfrom=dict2.top'%word)
-        # r=requests.get(url='https://www.collinsdictionary.com/zh/dictionary/english/%s'%word)
-        # 利用BeautifulSoup将获取到的文本解析成HTML
-        soup = BeautifulSoup(r.text, "lxml")
-        # 获取字典的标签内容
-        #测试
-        # s2=soup.find(class_='trans-container')
-        # print("trans-container:",s2)
-        output(soup)
- 
-        # print(soup)
-        print('='*40+'\n')
-    except Exception:
-        print("Sorry, there is a error!\n")
-    finally:
-        word = input( "Enter a word (enter 'q' to exit): ")
-print("soup",soup)
+    
+    # 存储单词词义
+    def storeTrans(self):
+        s = self.soup.find(class_='trans-container')('ul')[0]('li')
+        text=""
+        for item in s:
+            if item.text:
+                text=text+"\n"+item.text
+        self.dict["trans"]=text
+    # 存储例句及其翻译
+    def storeExamples(self):
+        exList=self.soup.find(class_="trans-container tab-content",id="bilingual")('ul')[0]('li')
+        ex1=exList[0]('p')
+        self.dict["examples"]=ex1[0].get_text()
+        self.dict["examples_trans"]=ex1[1].get_text()
+    def storePhonetic(self):
+        s=self.soup.find(class_="phonetic")
+        self.dict["phonetic"]=s.get_text()
+    def output(self):
+        if(self.soup!=None):
+            for i in self.dict.values():
+                print(i)
+    def isGetSoup(self):
+        if self != None:
+            return True
+        else:
+            return False
+    def getDict(self):
+        return self.dict
+    def __init__(self,input_word):
+        self.soup=None        
+        self.dict={"word":"单词","trans":"词义","phonetic":"音标","examples":"例句","examples_trans":"例句翻译"}
+        # get word from Command line
+        try:
+            # 利用GET获取输入单词的网页信息
+            r = requests.get(url='http://dict.youdao.com/w/%s/#keyfrom=dict2.top'%input_word)
+            # r=requests.get(url='https://www.collinsdictionary.com/zh/dictionary/english/%s'%word)
+            # 利用BeautifulSoup将获取到的文本解析成HTML
+            self.soup = BeautifulSoup(r.text, "lxml")
+            #设定词典单词信息
+            self.dict["word"]=input_word
+            print('='*40+'\n')
+        except Exception:
+            print("Sorry, there is a error!\n")
+
+
